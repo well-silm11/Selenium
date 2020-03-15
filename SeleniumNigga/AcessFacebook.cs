56a235b7c314524074b72ff5d24db39211b7aff2
Wellington Almeida <welli.meida@hotmail.com>
2020-03-15 20:38:57 -0300

    .Commit

---
+++ SeleniumNigga/AcessFacebook.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumNigga
+{
+    public class AcessFacebook
+    {
+
+
+        public static void RequestFacebook(List<string> fv)
+        {
+            ChromeOptions options = new ChromeOptions();
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.SuppressInitialDiagnosticInformation = true;
+
+            IWebDriver driver = null;
+           
+            using (driver = new ChromeDriver(service, options))
+            {
+            
+      
+                List<string> resul = new List<string>();
+                driver.Url="https://pt-br.facebook.com";
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.ClassName("login_form_label_field"));
+
+                var link = driver.FindElements(By.TagName("td"))[6];
+                
+                link.Click();
+
+                
+                Thread.Sleep(3000);
+                foreach (var item in fv)
+                {
+                    var count = 0;
+
+                    driver.FindElement(By.Id("identify_email")).SendKeys(item+Keys.Enter);
+                    Thread.Sleep(3000);
+
+                    IList<IWebElement> webs = driver.FindElements(By.XPath("//div[@class='fsl fwb fcb']")).ToList();
+
+
+
+                    if (webs[count].Text== "Nenhum resultado para a pesquisa")
+                    {
+                        Console.WriteLine("O E-mail ou Telefone: "+item+"  não retornou nenhum perfil\n Aperte qualquer tecla para ir para o próximo");
+                        Console.ReadKey();
+                    
+                    }
+                    else
+                    {
+                        Console.WriteLine("Perfil encontrado com "+item+ ", o nome é: "+webs[count].Text+ "\n Aperte qualquer tecla para ir para o próximo");
+                        Console.ReadKey();
+                        driver.FindElements(By.Name("reset_action"))[1].Click();
+                    }
+                    count++;
+                    Console.Clear();
+
+                }
+                Console.WriteLine("Pronto! todos os itens da lista em csv fora verificados, flw parça!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ SeleniumNigga/Program.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace SeleniumNigga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<string> email = null;
+            while (email==null)
+            {
+                Console.Clear();
+                var caminho = "";
+                Console.WriteLine("Olá!\n Digite o caminho onde se encontra o csv:\n");
+                caminho = Console.ReadLine();
+
+
+
+                email = ConvertSCV(caminho);
+            }
+
+            AcessFacebook.RequestFacebook(email);
+
+        }
+
+        private static List<string> ConvertSCV(string caminho)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(File.OpenRead(caminho));
+                List<string> listA = new List<String>();
+
+                while (!reader.EndOfStream)
+                {
+                    var a = 0;
+                    string line = reader.ReadLine();
+                    if (!String.IsNullOrWhiteSpace(line))
+                    {
+                        string[] values = line.Split(';');
+                        if (values.Length >= 2)
+                        {
+                            listA.Add(values[a]);
+                            a++;
+                            listA.Add(values[a]);
+                        }
+                    }
+                }
+                string[] firstlistA = listA.ToArray();
+
+                return listA.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Este caminho informado não retorna um csv válido, veirifique se está correto e tente novamente");
+                Console.ReadKey();
+                return null;
+            }
+        }
+    }
+}
